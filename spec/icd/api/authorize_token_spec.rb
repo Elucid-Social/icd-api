6b9b88a8d3a1e25e38954fbd641a03c32c42b327
Alejandro <janderl@gmail.com>
2023-08-09 23:17:34 +0200

    :cop:

--- spec/icd/api/authorize_token_spec.rb
+++ spec/icd/api/authorize_token_spec.rb
@@ -1,30 +1,36 @@
 # frozen_string_literal: true
 
-RSpec.describe Icd::Api::AuthorizeToken do
-  before :suite do
-    described_class.configure do |conf|
-      conf.client_id = 'client_id'
-      conf.client_secret = 'client_secret'
-    end
-  end
-  describe '#retrieve_access_token' do
-    subject { described_class.call }
+require 'spec_helper'
 
-    it 'sends request to token endpoint' do
-      VCR.use_cassette('icd/api/authorize_token') do
-        expect(subject).to be_a(Icd::Api::AccessToken)
+module Icd
+  module Api
+    RSpec.describe AuthorizeToken do
+      before :suite do
+        described_class.configure do |conf|
+          conf.client_id = 'client_id'
+          conf.client_secret = 'client_secret'
+        end
       end
-    end
+      describe '#retrieve_access_token' do
+        subject { described_class.call }
 
-    it 'returns access token with expire in 3600' do
-      VCR.use_cassette('icd/api/authorize_token') do
-        expect(subject.expires_at.to_i).to be_within(5).of(Time.now.to_i + 3600)
-      end
-    end
+        it 'sends request to token endpoint' do
+          VCR.use_cassette('icd/api/authorize_token') do
+            expect(subject).to be_a(Icd::Api::AccessToken)
+          end
+        end
 
-    it 'returns access token with expire in 3600' do
-      VCR.use_cassette('icd/api/authorize_token_expired') do
-        expect(subject.expires_at.to_i).to be_within(5).of(Time.now.to_i + 3600)
+        it 'returns access token with expire in 3600' do
+          VCR.use_cassette('icd/api/authorize_token') do
+            expect(subject.expires_at.to_i).to be_within(5).of(Time.now.to_i + 3600)
+          end
+        end
+
+        it 'returns access token with expire in 3600' do
+          VCR.use_cassette('icd/api/authorize_token_expired') do
+            expect(subject.expires_at.to_i).to be_within(5).of(Time.now.to_i + 3600)
+          end
+        end
       end
     end
   end
